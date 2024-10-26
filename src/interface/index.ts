7a15e9c4c5997ca924a25e666a1441a18f55deeb
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-26 23:50:43 +0800

    update

---
+++ src/interface/index.ts
@@ -0,0 +1,6 @@
+
+
+export interface GeneratedElement {
+  element: HTMLElement;
+  id: string | null;
+}
---
+++ src/interface/library/index.ts
@@ -0,0 +1,22 @@
+import { documentQuerySelector } from '../../tools/query-selector';
+import { SetObject } from '../../data/sets/index';
+import { generateIdentifier } from '../../tools/index';
+
+const libraryField = documentQuerySelector('.css_library_field');
+
+function generateSetElement(setObject: SetObject): GeneratedElement {
+  const id = generateIdentifier('s');
+  const element = document.createElement('div');
+  element.classList.add('css_library_set');
+  element.id = id;
+  element.innerHTML = `<div class="css_library_set_thumbnail">${setObject.thumbnail}</div><div class="css_library_set_name">${setObject.name}</div>`;
+  element.setAttribute('onclick', `drawlots2.sets.openSet('${setObject.id}')`);
+  return {
+    id: id,
+    element: element
+  };
+}
+
+function initializeLibrary(): void {
+  
+}
