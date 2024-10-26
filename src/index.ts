d1f496735ac6b8d98f798bdc4a7d3b360b86852c
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-27 00:51:54 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+/*
 import './interface/theme.css';
 import './interface/index.css';
 
 import './interface/library/field.css';
-
+*/
 window.drawlots2 = {
   initialize: function () {},
   sets: {}
