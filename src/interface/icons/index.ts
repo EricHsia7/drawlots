51a369850c5f27f539f9154077cec773782c177a
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-27 15:13:54 +0800

    update

---
+++ src/interface/icons/index.ts
@@ -0,0 +1,5 @@
+import { MaterialSymbols } from './material-symbols-type';
+
+export function getIconHTML(iconID: MaterialSymbols): string {
+  return `<span class="css_material_symbols_rounded">${iconID}</span>`;
+}
