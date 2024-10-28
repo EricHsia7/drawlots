d08efa7d339d18498563cc92dd905b29807a02ea
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-28 19:50:27 +0800

    update

--- src/tools/image.ts
+++ src/tools/image.ts
@@ -27,7 +27,7 @@ export async function getImageColors(dataURL: string): Promise<Array<Color>> {
       const img = new Image();
       img.onload = function () {
         const colorThief = new ColorThief();
-        const colors = colorThief.getColor(img).then((e) => {
+        colorThief.getColor(img).then((colors) => {
           let result = [];
           for (const color of colors) {
             result.push({ r: color[0], g: color[1], b: color[2] });
