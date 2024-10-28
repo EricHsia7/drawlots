ccf59cac333b85c16bf5e0df596633847da1c3a1
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-28 19:15:16 +0800

    update

--- src/tools/colors.ts
+++ src/tools/colors.ts
@@ -1,5 +1,22 @@
 export interface Color {
-  r: number
-  g: number
-  b: number
+  r: number;
+  g: number;
+  b: number;
+}
+
+export function predictTextColor(color: Color): number {
+  const value = (-0.002658900692 * color.r + 0.9726844531 + -0.004339532172 * color.g + 1.050614409 + -0.002601957336 * color.b + 0.9287718383) / 3;
+  if (value > 0.46) {
+    return {
+      r: 255,
+      g: 255,
+      b: 255
+    };
+  } else {
+    return {
+      r: 0,
+      g: 0,
+      b: 0
+    };
+  }
 }
