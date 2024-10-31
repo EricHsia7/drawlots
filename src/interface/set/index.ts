6d13762e9c7effcb184b11fcfd3d3ee58eeb9e20
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-10-31 18:16:26 +0800

    update

--- src/interface/set/index.ts
+++ src/interface/set/index.ts
@@ -4,6 +4,10 @@ import { documentQuerySelector, elementQuerySelector } from '../../tools/query-s
 import { closePreviousPage, fadeInElement, fadeOutElement, openPreviousPage, pushPageHistory, revokePageHistory } from '../index';
 
 const setField = documentQuerySelector('.css_set_field');
+const setBody = elementQuerySelector(setField, '.css_set_body');
+const drawFromSetCapsuleElement = elementQuerySelector(setBody, '.css_draw_from_set_capsule');
+const drawFromSetElement = elementQuerySelector(drawFromSetCapsuleElement, '.css_draw_from_set');
+const drawOptionsElement = elementQuerySelector(drawFromSetCapsuleElement, '.css_draw_options');
 const setHeadElement = elementQuerySelector(setField, '.css_set_head');
 const setHeadLeftButtonElement = elementQuerySelector(setHeadElement, '.css_set_button_left');
 const setHeadRightButtonElement = elementQuerySelector(setHeadElement, '.css_set_button_right');
@@ -23,4 +27,6 @@ export function closeSet(): void {
 export function initializeSetRipple(): void {
   addRippleTo(setHeadLeftButtonElement);
   addRippleTo(setHeadRightButtonElement);
+  addRippleTo(drawFromSetElement);
+  addRippleTo(drawOptionsElement);
 }
